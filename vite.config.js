3c0693192670d6b76a124d4610e7a026d2bc1139
Ori Shriki <49729039+Yodaswag@users.noreply.github.com>
2026-05-17 23:36:53 +0300

    build: compile output to docs folder for github pages

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/dino-game/',
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 })
